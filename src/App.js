d88e1d38f81d98a05929327523adb49fc36f83c0
Rabibahho <rabi.github@hotmail.com>
2023-09-05 16:22:32 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,69 @@
-import logo from './logo.svg';
-import './App.css';
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import React, { useState } from "react";
+const users = [];
+const App = () => {
+  const [name, setName] = useState("");
+  const [age, setAge] = useState("");
 
-function App() {
+  const handleName = (e) => {
+    e.preventDefault();
+    setName(e.target.value);
+  };
+  const handleAge = (e) => {
+    e.preventDefault();
+    setAge(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const input = { name: { name }, age: { age } };
+    users.push(input);
+    console.log(users);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ width: "400px", margin: "60px auto" }}>
+      <form onSubmit={handleSubmit}>
+        <div className="mb-3">
+          <label htmlFor="exampleInputEmail1" className="form-label">
+            Name
+          </label>
+          <input
+            type="text"
+            name="name"
+            className="form-control"
+            id="exampleInputEmail1"
+            aria-describedby="emailHelp"
+            onChange={handleName}
+          />
+        </div>
+
+        <div className="mb-3">
+          <label htmlFor="exampleInputPassword1" className="form-label">
+            Age
+          </label>
+          <input
+            type="text"
+            className="form-control"
+            id="exampleInputPassword1"
+            onChange={handleAge}
+          />
+        </div>
+        <div className="d-flex justify-content-center ">
+          <button
+            type="submit"
+            className="btn btn-primary d-flex justify-content-center col-6 "
+          >
+            Add
+          </button>
+        </div>
+        <div>
+          {users.map((users) => (
+            <p>User : {users.name}</p>
+          ))}
+        </div>
+      </form>
     </div>
   );
-}
-
+};
 export default App;
